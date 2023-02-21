dcde36ff2387269c4ae48aebbc8ea93e41108d56
vsuraj25 <55409076+vsuraj25@users.noreply.github.com>
2023-02-21 18:03:31 +0530

    App Updated

--- src/constant/__init__.py
+++
@@ -1,4 +0,0 @@
-connection_string = 'mongodb+srv://Average:Average@cluster0.qsyb3x2.mongodb.net/?retryWrites=true&w=majority'
-database_name = 'NEWS_ARTICLE_CLASSIFICATION'
-train_table = 'news_train_data'
-test_table = 'news_test_data'
